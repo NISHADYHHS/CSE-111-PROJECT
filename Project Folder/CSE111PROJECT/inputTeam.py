b0b3574105c6985d36c4341ed48d237e3c537662
VRzorua01 <148034869+VRzorua01@users.noreply.github.com>
2023-11-07 13:58:01 -0800

    Add files via upload

--- Project Folder/CSE111PROJECT/inputTeam.py
+++ Project Folder/CSE111PROJECT/inputTeam.py
@@ -17,17 +17,27 @@ def addPokemon(name, id, moves):
     cur = con.cursor()
     cur.execute("SELECT p_id FROM Pokemon WHERE p_name = '" + str(name) + "'")
     mon = cur.fetchall()[0][0]
+    moves = setMoves(moves)
     data = [
         (mon, 1, id, moves[0], moves[1], moves[2], moves[3])
     ]
     cur.executemany("INSERT INTO TrainerPokemon VALUES(?, ?, ?, ?, ?, ?, ?)", data)
     con.commit()
+    addCount(id)
 
-def addTeam(id):
+def addTeam(names, moves, id):
+    print(moves[0])
+    con = sqlite3.connect("pokemon.db")
+    cur = con.cursor()
+    for i in range(len(names)):
+        moves[i] = setMoves(moves[i])
+        addPokemon (names[i], id, moves[i])
+
+    '''
     con = sqlite3.connect("pokemon.db")
     cur = con.cursor()
     cur.execute("SELECT t_team_pokemon_count FROM Trainer WHERE t_id = '" + str(id) + "'")
-    print(amt)
+
     amt = cur.fetchall()[0][0]
     for i in range (0, amt):
         poke = input()
@@ -39,6 +49,50 @@ def addTeam(id):
             else:
                 moves.append(None)
         addPokemon(poke, id, moves)
+        '''
+
+def setMoves(moves):
+    for i in range(4):
+        if i < len(moves):
+            moves[i] = getMove(moves[i])
+        else:
+            moves.append(None)
+    return moves
+
+def getEnemyTrainer(t_id):
+    con = sqlite3.connect("pokemon.db")
+    cur = con.cursor()
+    cur.execute("SELECT * FROM Trainer INNER JOIN Location ON Location.l_id = Trainer.t_id Inner JOIN TrainerPokemon ON Pokemon.p_trainerID = Trainer.t_id WHERE Trainer.t_id = '" + str(t_id) + "'")
+    data = cur.fetchall()
+    return data
+
+def getPokeInfo(p_id):
+    con = sqlite3.connect("pokemon.db")
+    cur = con.cursor()
+    cur.execute("SELECT * FROM Pokemon INNER JOIN Location ON Location.l_id = Pokemon.p_LocationID Inner JOIN Ability ON a_id = p_ability1 WHERE Pokemon.p_id = '" + str(p_id) + "'")
+    data = cur.fetchall()
+    return data
+
+def getTrainerLocation(t_id):
+    con = sqlite3.connect("pokemon.db")
+    cur = con.cursor()
+    cur.execute("SELECT t_locationID FROM Trainer WHERE t_id = '" + str(t_id) + "'")
+    location = cur.fetchall()[0][0]
+    return location
+
+def getTrainers(t_name):
+    con = sqlite3.connect("pokemon.db")
+    cur = con.cursor()
+    cur.execute("SELECT t_id FROM Trainer WHERE t_name = '" + str(t_name) + "'")
+    trainers = cur.fetchall()
+    return trainers
+
+def getTrainerNames(t_id):
+    con = sqlite3.connect("pokemon.db")
+    cur = con.cursor()
+    cur.execute("SELECT t_name FROM Trainer WHERE t_id = '" + str(t_id) + "'")
+    trainer = cur.fetchall()[0][0]
+    return trainer
 
 def getMove(name):
     con = sqlite3.connect("pokemon.db")
@@ -57,11 +111,11 @@ def getPokemon(name):
 
 def deletePokemon(t_id, poke):
     con = sqlite3.connect("pokemon.db")
-
     cur = con.cursor()
     p_id = getPokemon(poke)
     cur.execute("DELETE FROM TrainerPokemon WHERE p_trainerID = '" + str(t_id) + "'" + " AND p_id = '" + str(p_id) + "'")
-    con.commit()  # Remember to commit the transaction after executing INSERT.
+    con.commit()
+    subCount(t_id)
 
 def deleteTeam(t_id):
     con = sqlite3.connect("pokemon.db")
@@ -91,9 +145,21 @@ def replacePokemon(name, poke, id, moves):
     else:
         print("not found\n")
 
+def addCount(id):
+    con = sqlite3.connect("pokemon.db")
+    cur = con.cursor()
+    cur.execute("UPDATE Trainer SET t_team_pokemon_count = t_team_pokemon_count + 1 WHERE t_id = '" + str(id) + "'")
+    con.commit()
+
+def subCount(id):
+    con = sqlite3.connect("pokemon.db")
+    cur = con.cursor()
+    cur.execute("UPDATE Trainer SET t_team_pokemon_count = t_team_pokemon_count - 1 WHERE t_id = '" + str(id) + "'")
+    con.commit()
+
 if __name__ == "__main__":
     #name = input()
     #team_cnt = input()
     #addPlayer(name, team_cnt)
     #addTeam(input("enter id"))
-    replacePokemon('Pikachu', 'Bulbasaur', 7, ['Razor Leaf', 'Thunderbolt'])
+    print(getTrainerLocation(2))
